04ce816227aa52c31bb9adaebfe5d8130096601a
ChiHao133 <chihao133713@gmail.com>
2026-07-03 14:16:40 +0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
+        foreach(char c in s){
+            if(c=='(' || c=='{'|| c=='[') stack.Push(c);
+            else {
+                if(stack.Count==0) return false;
+                char close = stack.Pop();
+                if(c==')' && close !='(' || c=='}' && close!='{'|| c==']' && close!= '[') 
+                return false;
+            }
+        }
+        return stack.Count()==0;
+    }
+}
